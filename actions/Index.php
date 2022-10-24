d7f2a11c4e3fefc754fc33804abd1725c11a5529
4X_Pro <me@4xpro.ru>
2022-10-24 03:41:19 +0300

    Added memory consumption output

--- actions/Index.php
+++ actions/Index.php
@@ -6,7 +6,8 @@ use function \MLWF\app, \MLFW\_dbg;
 class Index implements \MLFW\IAction {
   public function exec($params=null):\MLFW\Layouts\Basic {
     $l = new \MLFW\layouts\Basic;
-    $l->wrap('Hello world!');
+    $l->wrap('Hello world!'.PHP_EOL);
+    $l->wrap('Memory used: '.memory_get_peak_usage().' bytes');
     return $l;
   }
 }
