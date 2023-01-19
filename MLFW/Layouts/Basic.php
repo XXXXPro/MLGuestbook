fc11c8d9e823b9b64e2013ab2f901022306f8d7f
4X_Pro <me@4xpro.ru>
2023-01-19 22:24:26 +0300

    Headers output done

--- MLFW/Layouts/Basic.php
+++ MLFW/Layouts/Basic.php
@@ -12,11 +12,41 @@ namespace MLFW\layouts;
 
 class Basic {
   private $mime = 'text/plain';
-  private $modified =  null;
+  private $charset = 'utf-8';
+  private $last_modified =  null;
   private $objects = [];
   private $headers = [];
 
+  public function __construct() {
+    header_register_callback([$this,'headersOutput']);
+  }
+
+  public function setMime($mime):void {
+    $this->mime = $mime;
+  }
+
+  public function setCharset($charset):void {
+    $this->charset = $charset;
+  }
+
+  public function setHeader($name,$value):void {
+    $this->headers[$name]=$value;
+  }
+
+  public function addHeader($name,$value):void {
+    if (!empty($this->headers[$name])) $this->headers.=', '.$value;
+    else $this->headers[$name]=$value;
+  }
+
+  public function headersOutput():void {
+    if (empty($this->headers['Content-Type'])) $this->headers['Content-Type']=$this->mime.'; charset='.$this->charset;
+    // if (empty($this->headers['Last-Modified'])) $this->headers['Last-Modified']=empty($this->modified) ? date('') : ;
+    if (empty($this->headers['Content-Length'])) $this->headers['Content-Length']=ob_get_length();
+    foreach ($this->headers as $name=>$value) header($name.': '.$value);
+  }
+
   public function wrap($obj):void {
+    if ($obj instanceof \MLFW\Models\Entity && isset($obj->last_modified)) $this->last_modified=$obj->last_modified;
     $this->objects[]=$obj;
   }
 
