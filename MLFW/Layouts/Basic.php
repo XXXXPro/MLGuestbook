ee6345a831fa1a2be493db296a1c99edca7335ff
4X_Pro <me@4xpro.ru>
2023-02-02 00:48:13 +0300

    Template class added

--- MLFW/Layouts/Basic.php
+++ MLFW/Layouts/Basic.php
@@ -10,11 +10,10 @@
 
 namespace MLFW\layouts;
 
-class Basic {
+class Basic extends \MLFW\Template {
   private $mime = 'text/plain';
   private $charset = 'utf-8';
   private $last_modified =  null;
-  private $objects = [];
   private $headers = [];
 
   public function __construct() {
@@ -40,19 +39,14 @@ class Basic {
 
   public function headersOutput():void {
     if (empty($this->headers['Content-Type'])) $this->headers['Content-Type']=$this->mime.'; charset='.$this->charset;
-    // if (empty($this->headers['Last-Modified'])) $this->headers['Last-Modified']=empty($this->modified) ? date('') : ;
+    // if (empty($this->headers['Last-Modified'])) $this->headers['Last-Modified']=empty($this->modified) ? date('') : ; // TODO: add correct format output
     if (empty($this->headers['Content-Length'])) $this->headers['Content-Length']=ob_get_length();
     foreach ($this->headers as $name=>$value) header($name.': '.$value);
   }
 
-  public function wrap($obj):void {
-    if ($obj instanceof \MLFW\Models\Entity && isset($obj->last_modified)) $this->last_modified=$obj->last_modified;
-    $this->objects[]=$obj;
-  }
-
-  public function __toString():string  {
+  public function getTemplate(): string {
     $result = '';
-    foreach ($this->objects as $obj) $result.=$obj;
+    foreach ($this->subitems as $obj) $result.=$obj;
     return $result;
   }
 }
---
+++ MLFW/Template.php
@@ -0,0 +1,83 @@
+<?php
+/** ================================
+ *  @package MLFW
+ *  @author 4X_Pro <me@4xpro.ru>
+ *  @version 0.90
+ *  @url 
+ *  MindLife FrameWork basic template class
+ *  ================================ **/
+
+ namespace MLFW;
+
+ abstract class Template {
+  protected $data;
+  protected $subitems = [];
+  public function __construct(object $obj) {
+    $this->data = $obj; 
+  }
+
+  /** Wraps object with specified template class. 
+   * Template must inherit MLFW\Template class object.
+   * @param object $obj Object to wrap with template
+   * @param string $wrapper Wrapper class name
+   **/
+  public function wrap(object $obj, string $wrapper):void {
+    if (\class_exists($wrapper) && \is_subclass_of($wrapper,'\\MLFW\\Template',true)) { // Allow only Template subclasses for security reasons
+      $tmpl = new $wrapper($obj);
+      $this->subitems[]=$tmpl;
+    }
+    else {
+      _dbg("Class $wrapper not found!");
+    }
+  }
+
+  /** Adds text string to template 
+   * @param string $text Text to add
+   **/
+  public function wrapText(string $text):void {
+    $this->subitems[]=$text;
+  }
+
+  /** Wraps array of object with specified template class. 
+   * Template must inherit MLFW\Template class object.
+   * @param array $objs Array of objects to wrap with template
+   * @param string $wrapper Wrapper class name
+   **/  
+  public function wrapAll(array $objs, string $wrapper):void { 
+    foreach ($objs as $obj) $this->wrap($obj,$wrapper);
+  }
+
+  /** Calls specified callback function for each object and wraps it with template class returned by the function.
+   * Template must inherit MLFW\Template class object.
+   * @param array $objs Array of objects to wrap with template
+   * @param callable $cb Callback function. The function should accept one object from $objs array and return template class name as string.
+   **/    
+  public function wrapCallback(array $objs, callable $cb):void {
+    foreach ($objs as $obj) {
+      $this->wrap($obj,call_user_func($cb,$obj));
+    }
+  }
+
+  /** Calls specified method for each object and wraps it with template class returned by the method.
+   * Template must inherit MLFW\Template class object.
+   * @param array $objs Array of objects to wrap with template
+   * @param string $method Method to be called. It should return template class name as string.
+   **/    
+  public function wrapMethod(array $objs, string $method):void {
+    foreach ($objs as $obj) {
+      if (\method_exists($obj,$method)) $this->wrap($obj,$obj->$method());
+      else _dbg("Object has no method $method!");
+    }
+  }
+
+  /**  This function should return rendered template (for example, HTML code), which can be included to layout template or send directly to user.
+   * Usually it called from __toString function (__toString itself can't be abstract)
+   **/    
+  abstract public function getTemplate():string;
+
+  /** Conversion function, which usually called from parent object template or layout.
+   */
+  public function __toString():string {
+    return $this->getTemplate();
+  }
+ }
